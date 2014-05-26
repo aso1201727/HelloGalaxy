1f5a5f14b76aee849c0706a1f8108a9e3c4a3364
student <student@PC111S09.fgakusei.local>
2014-05-26 11:16:02 +0900

    アップデートです。

---
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.hellogalaxy;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener
+ {
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+		case R.id.btnOK: //btnMsgが押された
+			//エディットテキストから入力内容を取り出す
+			EditText etv = (EditText)findViewById(R.id.edtFirst);
+			String inputMsg = etv.getText().toString();
+			EditText etv2 = (EditText)findViewById(R.id.edtFamily);
+			String inputMsg2 = etv2.getText().toString();
+			
+			//メッセージ表示用のTextViewを探して、文字を設定
+			TextView tv = (TextView)findViewById(R.id.txvMsg);
+			tv.setText("あなたでしたか、" + inputMsg + inputMsg2 + "さん。");
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		//ボタンをIDで探してボタン変数を作る
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		//ボタン変数にリスナーを登録する
+		btnOK.setOnClickListener(this);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment())
+					.commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			return rootView;
+		}
+	}
+
+}
